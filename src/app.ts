31244bef695337d7ea24b174ece355693bdf5ae1
YangHyunYong <43340172+YangHyunYong@users.noreply.github.com>
2024-10-08 01:02:41 +0900

    subscribeEvent 추가

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,9 @@ const app = express();
 app.get("/status", (req, res, next) => {
     console.log("Request status API success!!");
 
-    res.send('GOOD!');
+    res.send('GOOD!');  //DB에서 select 한것 넘겨주기
 });
+
+
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import http from "http";
 import app from "./app";
+import { subscribeEvent } from "./subscribeEvent";
 
-const PORT = 5000;
+const PORT = 5001;
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
     console.log(`Start service on ${PORT} port!`);
+
+    subscribeEvent();
 });
